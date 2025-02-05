15c4a45210504f4532385700f09d07d184930e7b
mtunaz <90019202+mtunaz@users.noreply.github.com>
2025-02-05 16:55:07 +0700

    testing get product API from server, updated homepage

--- 8bitstore-fe/src/App.jsx
+++ 8bitstore-fe/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 import ProductPurchase from './components/ProductPurchase/ProductPurchase';
 import LoginForm from './components/LoginForm/LoginForm';
+import HomePage from './pages/HomePage/HomePage';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import ProductSection from './components/ProductSection/ProductSection';
 
 function App() {
 
   return (
     <>
-      <RegistrationForm />
+    <HomePage></HomePage>
+      <ProductSection />
     </>
   )
 }
